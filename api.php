b16387c0ad42461efdd7fcdd93d4122b2113a258
Marco Dieckhoff <dieck@gmx.de>
2016-12-31 12:18:59 +0100

    added PHP api

---
+++ api.php
@@ -0,0 +1,50 @@
+<?php
+
+header("Content-type: text/plain");
+
+$homematic = $_REQUEST["homematic"];
+if (!$homematic) die("ERR: No HomeMatic address");
+if (!preg_match('/^\w+[\w\.\-]*\w+$/', $homematic)) die("ERR: Invalid HomeMatic address");
+
+$port = $_REQUEST["port"];
+if (!$port) $port = 2001;
+if (!preg_match('/^\d+$/', $port)) $port = 2001;
+if (($port < 1) or ($port > 65534)) $port = 2001;
+
+$channel = $_REQUEST["channel"];
+if (!$channel) die("ERR: No channel");
+if (!preg_match('/^\d+$/', $channel)) die("ERR: Invalid channel (1)");
+if (($channel < 1) or ($channel > 50)) die("ERR: Invalid channel (2)");
+
+$press = $_REQUEST["press"];
+if (!$press) $press = "PRESS_SHORT";
+if (!(($press == "PRESS_SHORT") or ($press == "PRESS_LONG"))) $press = "PRESS_SHORT";
+
+$protocol = $_REQUEST["protocol"];
+if (!$protocol) $protocol = "http";
+if (!(($protocol == "http") or ($protocol == "https"))) $protocol = "http";
+
+$debug = $_REQUEST["debug"];
+if ($debug) {
+  print "protocol: $protocol\n";
+  print "homematic: $homematic\n";
+  print "port: $port\n";
+  print "channel: $channel\n";
+  print "press: $press\n";
+}
+
+$url = $protocol . "://" . $homematic . ":" . $port;
+
+require_once "phpxmlrpc-src/Autoloader.php";
+PhpXmlRpc\Autoloader::register();
+
+use PhpXmlRpc\Value;
+use PhpXmlRpc\Request;
+use PhpXmlRpc\Client;
+
+$client = new Client($url);
+$response = $client->send(new Request('setValue', array(new Value("BidCoS-RF:".$channel), new Value($press), new Value(True, 'boolean'))));
+
+print "done.\n"
+
+?>
---
+++ phpxmlrpc-src/README.md
@@ -0,0 +1,13 @@
+
+This is the src directory of the great php library for building xmlrpc
+clients and servers PHPXMLRPC, a project from Gaetano Giunta.
+
+Version 4.0.0 downloaded on Dec 31st, 2016.
+
+See https://github.com/gggeek/phpxmlrpc/
+and https://gggeek.github.io/phpxmlrpc
+
+It is published under the Software License Agreement (BSD License),
+see license.txt.
+
+
